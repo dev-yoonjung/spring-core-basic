b68813b718e75412fc3187a3d2b736492918da1f
dev-yoonjung <dear__yoon@naver.com>
2023-02-14 15:35:24 +0900

    feat: prototype scope

---
+++ src/test/java/hello/core/scope/PrototypeTest.java
@@ -0,0 +1,49 @@
+package hello.core.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class PrototypeTest {
+
+    @Test
+    void prototypeBeanFind() {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(PrototypeBean.class);
+
+        System.out.println("find prototype bean 1");
+        PrototypeBean prototypeBean1 = applicationContext.getBean(PrototypeBean.class);
+        System.out.println("find prototype bean 2");
+        PrototypeBean prototypeBean2 = applicationContext.getBean(PrototypeBean.class);
+
+        System.out.println("prototypeBean1 = " + prototypeBean1);
+        System.out.println("prototypeBean2 = " + prototypeBean2);
+
+        assertThat(prototypeBean1).isNotSameAs(prototypeBean2);
+
+        prototypeBean1.destroy();
+        prototypeBean2.destroy();
+        applicationContext.close();
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("PrototypeBean.destroy");
+        }
+
+    }
+
+}
